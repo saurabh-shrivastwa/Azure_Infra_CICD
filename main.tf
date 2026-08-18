19d318884c440718e7a73a7f141f4723f82b87ee
catchsaurabh001 <catchsaurabh001@gmail.com>
2026-08-18 06:24:32 +0530

    subnet code added

--- main.tf
+++ main.tf
@@ -15,5 +15,12 @@ resource "azurerm_virtual_network" "vnet" {
     name = "vnet1"
     location = "westus"
     resource_group_name = "rg1"
-    address_space = ["10.0.0.1"]
+    address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+    name = "subnet1"
+    resource_group_name = "rg1"
+    virtual_network_name = "vnet1"
+    address_prefixes = ["10.0.1.0"]
 }
